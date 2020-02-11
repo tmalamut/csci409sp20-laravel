540dbf721390cc6f60ebaf15e8a01d439477e4f7
Taylor Malamut <tfmalamut@coastal.edu>
2020-02-11 16:13:23 -0500

    Added Rooms Migration

---
+++ database/migrations/2020_02_11_205539_create_rooms_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateRoomsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('rooms', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->unsignedBigInteger('hotel_id');
+            $table->foreign('hotel_id')->references('id')->on('hotels');
+            $table->string('type');
+            $table->string('description');
+            $table->decimal('price', 10, 2); //need 10 digits with 2 to the right of the decimal
+            $table->string('image');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('rooms');
+    }
+}
