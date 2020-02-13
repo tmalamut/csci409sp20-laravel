b1d37015c3dc63b771deb151b9d781b850a64705
Taylor Malamut <tfmalamut@coastal.edu>
2020-02-13 12:39:55 -0500

    Added Reservations migration

---
+++ database/migrations/2020_02_11_205625_create_reservations_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateReservationsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('reservations', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->unsignedBigInteger('user_id');
+            $table->foreign('user_id')->references('id')->on('users');
+            $table->unsignedBigInteger('room_id');
+            $table->foreign('user_id')->references('id')->on('rooms');
+            $table->integer('num_of_guests');
+            $table->date('arrival');
+            $table->date('departure');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('reservations');
+    }
+}
