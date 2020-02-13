151a5533323020c6eb4a9441a43879fe284b7715
Taylor Malamut <tfmalamut@coastal.edu>
2020-02-13 12:30:00 -0500

    Added Reservation.php

---
+++ app/Models/Reservation.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Reservation extends Model
+{
+    //
+    protected $fillable = [
+        'user_id',
+        'room_id',
+        'num_of_guests',
+        'arrival',
+        'departure',
+    ];
+
+    public function room() {
+        return $this->belongsTo('App\Models\Room');
+    }//end room function
+
+    public function user() {
+        return $this->belongsTo('App\User');
+    }//end user function
+}
