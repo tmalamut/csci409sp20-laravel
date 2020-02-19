fea32220e85fd58b50109acac7fb3c5ae379194a
Taylor Malamut <tfmalamut@coastal.edu>
2020-02-19 12:25:56 -0500

    Created Users seeder

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -12,5 +12,10 @@ class UsersTableSeeder extends Seeder
     public function run()
     {
         //
+        DB::table('users')->insert([
+            'name' => Str::random(10),
+            'email' => Str::random(10).'@gmail.com',
+            'password' => bcrypt('password'),
+        ]);
     }
 }
