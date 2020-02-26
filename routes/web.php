140be5aacb1058497b0d41380fac33f64d8896cd
Taylor Malamut <tfmalamut@coastal.edu>
2020-02-26 17:20:01 -0500

    Updated web.php

--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,51 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+//Replaced with new route
+Route::get('/', function(){
+    return '/ route';
 });
+
+//Get route hotels that returns the message /hotels route
+Route::get('hotels', function(){
+    return '/hotels route';
+});
+
+//Create a group of routes prefixed with reservations
+Route::prefix('reservations')->group(function () {
+    //Get Routes
+    Route::get('/', function () {
+        return 'Showing users homepage';
+    });
+    Route::get('reservations', function () {
+        return 'Showing users reservations';
+    });
+    Route::get('reservations/new', function () {
+        return 'Showing form to create reservations';
+    });
+    Route::get('reservations/{id}', function () {
+        return 'Showing reservation id';
+    });
+    Route::get('reservations/{id}/edit', function () {
+        return 'Showing edit form for reservation id';
+    });
+    //Post routes
+    Route::post('reservations', function () {
+        return 'Creating reservation';
+    });
+    //Put requests
+    Route::put('reservations/{id}', function () {
+        return 'Updating reservation id';
+    });
+    //Delete requests
+    Route::delete('reservations/{id}', function () {
+        return 'Deleting reservation id';
+    });
+});
+
+
+
+
+
+
+
